c8c7007339d5b7d013a6f83d7e5b7836ebe3d48b
S.S <ss@ss.com>
2016-10-19 07:23:47 +0900

    arg validation testcode

--- arg.go
+++ arg.go
@@ -1,13 +1,59 @@
 package movieconverter
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"gopkg.in/go-playground/validator.v9"
+)
+
 // Arg プログラム引数
 type Arg struct {
 	// TargetDir 監視対象ディレクトリ
-	TargetDir string
+	TargetDir string `validate:"required,max=256"` // [MEMO]（他のパス系もだけど）厳密にやるなら、ファイルパスとして正しいかのチェックも必要
 	// SleepSecond 監視間隔（秒）
-	SleepSecond int
+	SleepSecond int `validate:"required"` // [MEMO]最大値のチェックは入れた方がいいかな
 	// MovieSuffix 監視対象動画ファイル拡張子群
-	MovieSuffix []string
+	MovieSuffix []string `validate:"required"` // [MEMO]厳密にやるなら mp4 など取りうる値を定義してチェック
 	// OutputDir 変換結果出力先ディレクトリ
-	OutputDir string
+	OutputDir string `validate:"required,max=256"`
+	// LogDir ログ出力先ディレクトリ
+	LogDir string `validate:"required,max=256"`
+}
+
+// NewArg ...
+func NewArg(targetDir string, sleepSecond string, movieSuffix string, outputDir string, logDir string) (*Arg, error) {
+
+	iSleepSecond, err := strconv.Atoi(sleepSecond)
+	if err != nil {
+		return nil, fmt.Errorf("プログラム引数(sleepSecond)のバリデーションエラー [ERROR] %s\n", err)
+	}
+
+	aryMovieSuffix := strings.Split(movieSuffix, ",")
+	// [MEMO]空文字をスプリットした結果のスライス要素数は０ではないみたい・・・。
+	for _, msfx := range aryMovieSuffix {
+		if msfx == "" {
+			return nil, fmt.Errorf("プログラム引数(movieSuffix)のバリデーションエラー [ERROR] Key: 'Arg.MovieSuffix' Error:Field validation for 'MovieSuffix' failed on the 'required' tag\n")
+		}
+	}
+
+	arg := Arg{
+		TargetDir:   targetDir,
+		SleepSecond: iSleepSecond,
+		MovieSuffix: aryMovieSuffix,
+		OutputDir:   outputDir,
+		LogDir:      logDir,
+	}
+
+	validate := validator.New()
+	err = validate.Struct(arg)
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			return nil, fmt.Errorf("プログラム引数のバリデーションエラー [ERROR] %s\n", err.(*validator.InvalidValidationError))
+		}
+		return nil, fmt.Errorf("プログラム引数のバリデーションエラー [ERROR] %s\n", err.(validator.ValidationErrors))
+	}
+
+	return &arg, nil
 }
--- cmd/movieconverter/main.go
+++ cmd/movieconverter/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"os"
-	"strings"
 
 	mc "github.com/sky0621/work-movieconverter"
 )
@@ -13,7 +12,10 @@ var version = "0.1.0"
 
 func main() {
 	log.Println("[START]movieconverter")
-	arg := parseFlag()
+	arg, err := parseFlag()
+	if err != nil {
+		os.Exit(1) // おそらくバリデーションに失敗
+	}
 	if arg == nil {
 		os.Exit(0)
 	}
@@ -21,29 +23,26 @@ func main() {
 	log.Println("[END]movieconverter")
 }
 
-func parseFlag() *mc.Arg {
+func parseFlag() (*mc.Arg, error) {
 	var showVersion bool
 
 	var targetDir string
-	var sleepSecond int
+	var sleepSecond string
 	var movieSuffix string
 	var outputDir string
+	var logDir string
 
 	flag.BoolVar(&showVersion, "v", false, "バージョン")
 	flag.StringVar(&targetDir, "t", ".", "監視対象ディレクトリ")
-	flag.IntVar(&sleepSecond, "s", 60, "監視間隔（秒）")
+	flag.StringVar(&sleepSecond, "s", "60", "監視間隔（秒）")
 	flag.StringVar(&movieSuffix, "x", "mp4", "監視対象動画ファイル拡張子群")
 	flag.StringVar(&outputDir, "o", "out", "変換結果出力先ディレクトリ")
+	flag.StringVar(&logDir, "l", ".", "ログ出力先ディレクトリ")
 	flag.Parse()
 	if showVersion {
 		log.Println("version:", version)
-		return nil
+		return nil, nil
 	}
 
-	return &mc.Arg{
-		TargetDir:   targetDir,
-		SleepSecond: sleepSecond,
-		MovieSuffix: strings.Split(movieSuffix, ","),
-		OutputDir:   outputDir,
-	}
+	return mc.NewArg(targetDir, sleepSecond, movieSuffix, outputDir, logDir)
 }
