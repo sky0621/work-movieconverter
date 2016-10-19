2cad2d8b493530e60125590c49d0ab03e1359140
S.S <ss@ss.com>
2016-10-19 15:02:46 +0900

    pending

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # work-movieconverter
+
+やりたいことに対して過剰に実装し始めたため、このブランチでは作業中断。（ソース自体は別ツール作成時に参考にするため残す）
+
+下記にてミニマム仕様で再実装。
+
+https://github.com/sky0621/work-go-movieconverter
--- movieconverter.go
+++ movieconverter.go
@@ -6,8 +6,9 @@ import (
 )
 
 // 指定のディレクトリ（※指定なしはカレント）を監視（指定秒間隔（※指定なしは60秒）でポーリング）し、
-// 配下の動画ファイル（★拡張子は要検討）のCREATE/UPDATE/DELETEがあれば、
+// 配下の動画ファイル（★拡張子は要検討）のCREATE/UPDATEがあれば、
 // 外部コマンド「ffmpeg」（★指定可能パラメータは要検討）を実行して、指定の出力先（※指定なしはカレント直下の out ディレクトリ）に変換結果を出力する。
+// 変換元の動画ファイルは削除する。（容量圧迫回避のため）
 
 // Run ...
 func Run(arg *Arg) {
@@ -18,7 +19,6 @@ func Run(arg *Arg) {
 		if perr != nil {
 			return
 		}
-		log.Println(previousList)
 
 		// 指定ディレクトリ配下の動画ファイルの一覧から動画ファイルリストを取得
 		curr := CurrentMovieList{TargetDir: arg.TargetDir, MovieSuffix: arg.MovieSuffix}
@@ -26,8 +26,23 @@ func Run(arg *Arg) {
 		if cerr != nil {
 			return
 		}
-		log.Println(currentList)
 
+		if len(currentList.MovieFiles) == 0 {
+			log.Println("変換対象の動画ファイルがまったく置かれていない")
+			goto GOTO_SLEEP
+		}
+
+		log.Println(previousList)
+		// if len(previousList.MovieFiles) == 0 {
+		// 	log.Println("初回、ないし、prev.listを削除してしまったケース")
+		// }
+
+		for _, movieFile := range currentList.MovieFiles {
+			log.Println(movieFile)
+			go runConvertMovies()
+		}
+
+	GOTO_SLEEP:
 		time.Sleep(time.Duration(arg.SleepSecond) * time.Second)
 	}
 }
